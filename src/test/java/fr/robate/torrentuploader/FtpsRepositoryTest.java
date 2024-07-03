883d84ce29f0054ed6e84e6e50e2fd6c6aebbcf3
Manurobate <manurobate@msn.com>
2024-07-03 10:30:49 +0200

    add log during test

--- src/test/java/fr/robate/torrentuploader/FtpsRepositoryTest.java
+++ src/test/java/fr/robate/torrentuploader/FtpsRepositoryTest.java
@@ -1,6 +1,7 @@
 package fr.robate.torrentuploader;
 
 import fr.robate.torrentuploader.configuration.FtpProperties;
+import fr.robate.torrentuploader.exception.NetworkError;
 import fr.robate.torrentuploader.exception.NoConnection;
 import fr.robate.torrentuploader.repository.FtpsRepository;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +17,7 @@ import org.apache.ftpserver.usermanager.impl.BaseUser;
 import org.apache.ftpserver.usermanager.impl.WritePermission;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,7 +28,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -78,9 +79,6 @@ class FtpsRepositoryTest {
         ftpServer = serverFactory.createServer();
         ftpServer.start();
 
-        // Wait for the server to be fully started
-        TimeUnit.SECONDS.sleep(5);
-
         log.debug("FTP server started on port {}", ftpProperties.getPort());
     }
 
@@ -101,17 +99,21 @@ class FtpsRepositoryTest {
         log.debug("FTP server stopped and temporary directory cleaned up.");
     }
 
-//    @BeforeEach
-//    public void init() throws NetworkError {
-//        // Ensure the repository is disconnected before each test
-//        try {
-//            if (ftpsRepository.isConnected()) {
-//                ftpsRepository.disconnect();
-//            }
-//        } catch (Exception e) {
-//            log.warn("Error during init disconnect: {}", e.getMessage());
-//        }
-//    }
+    @BeforeEach
+    public void init() throws NetworkError {
+        log.debug("Begin init test isConnected");
+
+        // Ensure the repository is disconnected before each test
+        try {
+            if (ftpsRepository.isConnected()) {
+                ftpsRepository.disconnect();
+            }
+        } catch (Exception e) {
+            log.warn("Error during init disconnect: {}", e.getMessage());
+        }
+        
+        log.debug("End init test isConnected");
+    }
 
     @Test
     void testConnectAndDisconnect() {
